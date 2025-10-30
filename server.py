e2459022ceefe0621f6f65fd00edf094db8806cd
0xh7 <fjdjfjd1424@gmail.com>
2025-10-31 00:33:50 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,17 @@
 from fastapi import FastAPI, WebSocket
-import chess
 
 app = FastAPI()
-rooms = {}
+players = []
 
-@app.websocket("/play/{room_id}")
-async def play(websocket: WebSocket, room_id: str):
+@app.websocket("/play")
+async def play(websocket: WebSocket):
     await websocket.accept()
-    if room_id not in rooms:
-        rooms[room_id] = {"players": []}
-    rooms[room_id]["players"].append(websocket)
+    players.append(websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            for p in rooms[room_id]["players"]:
+            for p in players:
                 if p != websocket:
                     await p.send_text(data)
     except:
-        rooms[room_id]["players"].remove(websocket)
+        players.remove(websocket)
